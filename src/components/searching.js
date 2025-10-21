1cfc1b584efe946055c8642e5c164993b91203b7
Konut <n.suxanow2015@yandex.ru>
2025-10-21 22:03:28 +0300

    Умная таблица

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,24 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import {createComparison, rules} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison(
+        ['skipEmptyTargetValues'],
+        [
+            rules.searchMultipleFields(
+                searchField,
+                ['date', 'customer', 'seller'],
+                false
+            )
+        ]
+    );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        const filteredData = data.filter(row => {
+            const result = compare(row, state);
+            return result;
+        });
+        return filteredData;
     }
 }
