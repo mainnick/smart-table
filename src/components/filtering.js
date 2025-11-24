e628de5fd4e211364a0ed73c51e7676d64b40aea
Konut <n.suxanow2015@yandex.ru>
2025-11-24 21:40:31 +0300

    Умная таблица v2.0

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,24 +1,16 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules); 
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                                   // Получаем ключи из объекта
-      .forEach((elementName) => {                                         // Перебираем по именам
-        elements[elementName].append(                                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])                      // формируем массив имён, значений опций
-                .map(name => {                                         // используйте name как значение и текстовое содержимое  
-                    const option = document.createElement('option');  // @todo: создать и вернуть тег опции
-                    option.value = name;
-                    option.textContent = name;
-                    return option;
-                })
-        )
-    }); 
-
-    return (data, state, action) => {
+    const applyFiltering = (query, state, action) => {
         // @todo: #4.2 — обработать очистку поля
         if (action && action.name === 'clear') {
             const field = action.dataset.field;
@@ -30,12 +22,20 @@ export function initFiltering(elements, indexes) {
         }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => {
-            const filterState = {
-                ...state,
-                total: [state.totalFrom, state.totalTo]
-            };
-            return compare(row, filterState);
-        }); 
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
+                    filter[`filter[${elements[key].name}]`] = elements[key].value;
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
